9090659aaee142d6b68f31d872d27f58ed6af7d8
Amr-3009 <amrosama1k@gmail.com>
2023-08-30 10:39:15 -0700

    gg

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,3 +20,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	return (total);
 }
+
+/**
+ * binary_tree_height - gets height of tree
+ * @tree: ip1
+ * Return: always
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t le = 0;
+	size_t ri = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			le = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			ri = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((le > ri) ? le : ri);
+	}
+}
