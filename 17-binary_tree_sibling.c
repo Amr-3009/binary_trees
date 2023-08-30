f9e7afcc581fff1658d1cf5ed45e76d545fae413
Amr-3009 <amrosama1k@gmail.com>
2023-08-30 10:59:39 -0700

    gg

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/***
+ * binary_tree_sibling - func finds sibling
+ * @node: ip1
+ * Return: always
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
