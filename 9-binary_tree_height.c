e283e3bb9cfc7fff9ab03ab0b3286a65078d1e85
Amr-3009 <amrosama1k@gmail.com>
2023-08-29 20:24:02 -0700

    gg

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,8 +8,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left = 0;
-	size_t right = 0;
+	size_t le = 0;
+	size_t ri = 0;
 
 	if (tree == NULL)
 	{
@@ -19,9 +19,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			le = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			ri = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((left > right) ? left : right);
+		return ((le > ri) ? le : ri);
 	}
 }
