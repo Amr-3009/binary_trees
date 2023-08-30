dcb5c7b2c6857b5a0b38448aeb9283328ef1a45f
Amr-3009 <amrosama1k@gmail.com>
2023-08-29 20:01:13 -0700

    hh

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - goes through tree using inoder
+ * @tree: ip1
+ * @func: ip2
+ * Return: always
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
