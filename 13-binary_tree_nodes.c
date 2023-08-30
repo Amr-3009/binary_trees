e9a8abd28e00cf2ed50ab4f9b50d63cfa3160214
Amr-3009 <amrosama1k@gmail.com>
2023-08-30 09:18:26 -0700

    gg

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - gets number of nodes
+ * @tree: ip1
+ * Return: always
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t node_count = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		node_count = node_count + ((tree->left || tree->right) ? 1 : 0);
+		node_count = node_count + binary_tree_nodes(tree->left);
+		node_count = node_count + binary_tree_nodes(tree->right);
+		return (node);
+	}
+}
